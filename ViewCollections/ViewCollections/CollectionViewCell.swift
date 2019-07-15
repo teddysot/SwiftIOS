cfbd13813636b2d18791e83643151020184b009e
Saharat Nasahachart <earth_sot@hotmail.com>
2019-07-15 15:12:52 -0700

    View Collection

---
+++ ViewCollections/ViewCollections/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  ViewCollections
+//
+//  Created by Teddy Nasahachart on 2019-07-10.
+//  Copyright © 2019 Teddy Nasahachart. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
--- ViewCollections/ViewCollections/ViewController.swift
+++ ViewCollections/ViewCollections/ViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let alphaBets = ["A","B","C","D","E","F","G","H", "I", "J", "K", "L", "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return alphaBets.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        
+        cell.label.text = alphaBets[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
